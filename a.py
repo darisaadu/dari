1cfac6f40f509552d76c5f330429ae9528c53eee
darisaadu <darisaadu2@gmail.com>
2021-05-13 05:42:45 -0700

    modified:   a.py

--- a.py
+++ a.py
@@ -1,14 +1,1 @@
- import numpy as np 
- import pandas as pd
- 
- df = pd.DataFrame({
-     "Name": ["Braund, Mr. Owen Harris",
-     "Allen, Mr. William Henry",
-     "Bonnell, Miss. Elizabeth"],
-     "Age": [22, 35, 58],
-     "Sex": ["male", "male", "female"]} 
-     )
-
-
-print(df)
-
+print('hello world')
